6cd4364c1b2b184ff63aeb8e2ba6f1f6d98ff98b
KesvaL <jabeddou@gmail.com>
2023-07-07 18:28:25 +0200

    🏗️ Move user and its store to own folder

---
+++ src/lib/auth/stores.ts
@@ -0,0 +1,41 @@
+import type { Auth, User } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { readable } from 'svelte/store';
+import { sdk } from '$lib/stores.js';
+
+/**
+ * @param {Auth} auth - The Firebase Auth instance.
+ * @returns A (subscribe-only) store for the current user's data.
+ */
+export function createUserStore(auth: Auth) {
+	let unsubscribe: () => void;
+
+	if (!auth) {
+		if (!globalThis.window) {
+			const { subscribe } = readable(null);
+			return {
+				subscribe
+			};
+		}
+		console.warn(
+			'%c[FIREBASE-SV] %cAuth was not initialized.',
+			'color: #ff3e00; font-weight: bold;',
+			'color: initial;'
+		);
+
+		const { subscribe } = readable(null);
+		return {
+			subscribe
+		};
+	}
+
+	const { subscribe } = readable<User | null | undefined>(auth?.currentUser ?? undefined, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return { subscribe };
+}
