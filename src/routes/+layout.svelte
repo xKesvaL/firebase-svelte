567ea868368447667e4fb67723aa6a52ef40578b
xKesvaL <jabeddou@gmail.com>
2023-09-30 15:07:50 +0200

    feat: shadcn & fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '@kesval/design';
+	import '$docs/styles/inter.scss';
+	import '$docs/styles/poppins.scss';
+    import '$docs/styles/main.scss';
+	import '../app.postcss';
 </script>
 
 <slot />
