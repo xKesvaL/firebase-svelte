ecf6a1384f6e4651d1ac2fd470c25f741f7a7aa1
xKesvaL <jabeddou@gmail.com>
2023-10-01 11:40:59 +0200

    fix: toasts position

--- src/docs/containers/layout/Toasts.svelte
+++ src/docs/containers/layout/Toasts.svelte
@@ -3,7 +3,7 @@
 	import { fly } from 'svelte/transition';
 </script>
 
-<div class="pointer-events-none fixed bottom-0 right-0 top-0 flex h-screen flex-col gap-2 p-4">
+<div class="pointer-events-none fixed bottom-0 right-0 top-20 flex h-screen flex-col gap-2 p-4">
 	{#each $toasts as toast, i}
 		<div
 			class="rounded-lg bg-background/80 p-4 shadow-lg shadow-foreground/10 backdrop-blur-md transition-transform"
