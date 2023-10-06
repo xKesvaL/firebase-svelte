07027c3aafcca4a38bea63e14a19b97c6ac36578
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2023-10-06 10:20:45 +0000

    feat: remote config docs 1

---
+++ src/routes/remote-config/stores/remote-config-boolean/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	import Code from '$docs/components/Code.svelte';
+	import { Button } from '$docs/ui/button';
+</script>
+
+<section>
+	<div class="container flex flex-col gap-8 py-8 lg:py-16 xl:py-24">
+		<h1 class="font-mono text-4xl font-black lg:text-5xl">createRemoteConfigBooleanStore()</h1>
+		<p class="max-w-prose">
+			<code>createRemoteConfigBooleanStore(remoteConfig, key, options)</code> creates a store that will
+			activate the current remoteconfig instance. The store contains a boolean value retrieved from the
+			remote config.
+		</p>
+		<h2 class="text-3xl lg:text-4xl">Parameters</h2>
+		<ul class="flex max-w-prose flex-col gap-4">
+			<li>
+				<code>remoteConfig: RemoteConfig</code> - The remote config instance. If you put the <Button
+					variant="link"
+					class="h-auto p-0 text-base text-accent">&lt;FirebaseApp&gt;</Button
+				> component at the root of your app, or higher than where you're using this store, you can use
+				<code>null</code> or <code>undefined</code> as the parameter and the store will retrieve the
+				database instance from the context.
+			</li>
+			<li>
+				<code>key: string</code> - The remote config key.
+			</li>
+			<li>
+				<code>options?: RemoteConfigStoreOptions&lt;boolean&gt;</code>
+			</li>
+		</ul>
+		<h2 class="text-3xl lg:text-4xl">Properties</h2>
+		<ul class="flex max-w-prose flex-col gap-4">
+			<li>
+				<code>subscribe: ((val: boolean) => void)) => void</code> - Subscribe method for the Svelte store.
+				Allows for the `$store` syntax.
+			</li>
+			<li>
+				<code>loading: boolean</code> - When the remote config key is loading.
+			</li>
+			<li>
+				<code>error: unknown</code> - Error if any.
+			</li>
+		</ul>
+		<h2>Types</h2>
+		<Code filename="" language="javascript">
+			{`interface RemoteConfigStoreOptions&lt;T&gt; {
+  subscribe: (val: boolean) => void;
+  loading: boolean;
+  error: unknown;
+}`}
+		</Code>
+	</div>
+</section>
+
+<style lang="scss">
+	section {
+		background: radial-gradient(circle at 28% 37%, hsl(var(--primary-300) / 0.25), transparent 50%),
+			radial-gradient(circle at 70% 66%, hsl(var(--secondary-300) / 0.25), transparent 50%);
+
+		ul {
+			li {
+				position: relative;
+				padding-left: 1.5rem;
+
+				&::before {
+					content: '•';
+					position: absolute;
+					left: 0;
+				}
+			}
+		}
+	}
+</style>
---
+++ src/routes/remote-config/stores/remote-config/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Code from '$docs/components/Code.svelte';
+	import { Button } from '$docs/ui/button';
+</script>
+
+<section>
+	<div class="container flex flex-col gap-8 py-8 lg:py-16 xl:py-24">
+		<h1 class="font-mono text-4xl font-black lg:text-5xl">createRemoteConfigActivationStore()</h1>
+		<p class="max-w-prose">
+			<code>createRemoteConfigActivationStore&lt;T&gt;(remoteConfig)</code> creates a store that will
+			activate the current remoteconfig instance. The store contains a boolean value that will be true
+			if the remote config is activated, and false if it's not.
+		</p>
+		<h2 class="text-3xl lg:text-4xl">Parameters</h2>
+		<ul class="flex max-w-prose flex-col gap-4">
+			<li>
+				<code>remoteConfig: RemoteConfig</code> - The remote config instance. If you put the <Button
+					variant="link"
+					class="h-auto p-0 text-base text-accent">&lt;FirebaseApp&gt;</Button
+				> component at the root of your app, or higher than where you're using this store, you can use
+				<code>null</code> or <code>undefined</code> as the parameter and the store will retrieve the
+				database instance from the context.
+			</li>
+		</ul>
+		<h2 class="text-3xl lg:text-4xl">Properties</h2>
+		<ul class="flex max-w-prose flex-col gap-4">
+			<li>
+				<code>subscribe: ((val: boolean) => void)) => void;</code> - Subscribe method for the Svelte
+				store. Allows for the `$store` syntax.
+			</li>
+		</ul>
+	</div>
+</section>
+
+<style lang="scss">
+	section {
+		background: radial-gradient(circle at 28% 37%, hsl(var(--primary-300) / 0.25), transparent 50%),
+			radial-gradient(circle at 70% 66%, hsl(var(--secondary-300) / 0.25), transparent 50%);
+
+		ul {
+			li {
+				position: relative;
+				padding-left: 1.5rem;
+
+				&::before {
+					content: '•';
+					position: absolute;
+					left: 0;
+				}
+			}
+		}
+	}
+</style>
